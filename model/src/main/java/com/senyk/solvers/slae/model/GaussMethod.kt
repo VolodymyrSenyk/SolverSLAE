671636828732a24f8440740e1d4d1e3bb9c6650e
Volodymyr Senyk <Volodymyr_Senyk@epam.com>
2019-09-15 19:29:34 +0300

    SolverSLAE - Model implemented

--- model/src/main/java/com/senyk/solvers/slae/model/GaussMethod.kt
+++ model/src/main/java/com/senyk/solvers/slae/model/GaussMethod.kt
@@ -1,7 +1,58 @@
 package com.senyk.solvers.slae.model
 
+import kotlin.math.abs
+
 class GaussMethod : Algorithm() {
     override fun solve(matrix: Array<DoubleArray>): DoubleArray {
-        return doubleArrayOf(1.1, 1.2, 1.3)
+        val A = Array(matrix.size) { DoubleArray(matrix.size) }
+        val B = DoubleArray(matrix.size)
+        for (i in matrix.indices) {
+            for (j in matrix[0].indices) {
+                if (j != matrix[0].size - 1) {
+                    A[i][j] = matrix[i][j]
+                } else {
+                    B[i] = matrix[i][j]
+                }
+            }
+        }
+        for (k in matrix.indices) {
+            // find pivot row
+            var max = k
+            for (i in k + 1 until matrix.size) {
+                if (abs(A[i][k]) > abs(A[max][k])) {
+                    max = i
+                }
+            }
+            // swap row in A matrix
+            val temp = A[k]
+            A[k] = A[max]
+            A[max] = temp
+
+            // swap corresponding values in constants matrix
+            val t = B[k]
+            B[k] = B[max]
+            B[max] = t
+
+            // pivot within A and B
+            for (i in k + 1 until matrix.size) {
+                val factor = A[i][k] / A[k][k]
+                B[i] -= factor * B[k]
+                for (j in k until matrix.size) {
+                    A[i][j] -= factor * A[k][j]
+                }
+            }
+        }
+
+        // Print row echelon form back substitution
+        val result = DoubleArray(matrix.size)
+        for (i in matrix.indices.reversed()) {
+            var sum = 0.0
+            for (j in i + 1 until matrix.size) {
+                sum += A[i][j] * result[j]
+            }
+            result[i] = (B[i] - sum) / A[i][i]
+        }
+        // Print solution
+        return result
     }
 }
--- model/src/main/java/com/senyk/solvers/slae/model/GaussSeidelMethod.kt
+++ model/src/main/java/com/senyk/solvers/slae/model/GaussSeidelMethod.kt
@@ -1,7 +1,41 @@
 package com.senyk.solvers.slae.model
 
+import kotlin.math.abs
+
 class GaussSeidelMethod : Algorithm() {
     override fun solve(matrix: Array<DoubleArray>): DoubleArray {
-        return doubleArrayOf(1.11, 1.22, 1.33)
+        var result = DoubleArray(matrix.size) { 0.0 }
+        while (true) {
+            val currentVariableValues = DoubleArray(matrix.size)
+            for (i in matrix.indices) {
+                currentVariableValues[i] = matrix[i][matrix.size]
+                for (j in matrix.indices) {
+                    if (i > j) {
+                        currentVariableValues[i] -= matrix[i][j] * currentVariableValues[j]
+                    }
+                    if (i < j) {
+                        currentVariableValues[i] -= matrix[i][j] * result[j]
+                    }
+                }
+                currentVariableValues[i] /= matrix[i][i]
+            }
+            var error = 0.0
+            for (aMatrix in matrix) {
+                var errorTemp = 0.0
+                for (j in matrix.indices) {
+                    errorTemp += aMatrix[j] * currentVariableValues[j]
+                }
+                error += abs(errorTemp - aMatrix[matrix.size])
+            }
+            if (error < EPS) {
+                break
+            }
+            result = currentVariableValues
+        }
+        return result
+    }
+
+    companion object {
+        const val EPS = 0.05
     }
 }
