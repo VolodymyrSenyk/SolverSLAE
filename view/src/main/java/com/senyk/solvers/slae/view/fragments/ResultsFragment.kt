0c937ab2cf7634ddd53e544df3ef9e00d1e0cca0
Volodymyr Senyk <Volodymyr_Senyk@epam.com>
2019-09-16 00:14:29 +0300

    SolverSLAE - UI improved

--- view/src/main/java/com/senyk/solvers/slae/view/fragments/ResultsFragment.kt
+++ view/src/main/java/com/senyk/solvers/slae/view/fragments/ResultsFragment.kt
@@ -1,15 +1,16 @@
 package com.senyk.solvers.slae.view.fragments
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.senyk.solvers.slae.R
-import com.senyk.solvers.slae.view.adapters.ResultsAnswersAdapter
 import com.senyk.solvers.slae.view.adapters.ResultsMatrixAdapter
 import kotlinx.android.synthetic.main.fragment_results.*
+import kotlinx.android.synthetic.main.result.view.*
 
 class ResultsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
@@ -31,27 +32,18 @@ class ResultsFragment : Fragment() {
         dataList.layoutManager = layoutManager
         matrix.adapter = ResultsMatrixAdapter(solverData.getSerializable(INPUT_DATA) as Array<DoubleArray>)
 
-        val answersList = results
-        answersList.setHasFixedSize(true)
-        val layoutManagerForAnswersList = LinearLayoutManager(requireActivity().applicationContext)
-        answersList.layoutManager = layoutManagerForAnswersList
-        val results = solverData.getDoubleArray(RESULT_DATA)
-        var countOfLines = results!!.size / NUM_OF_RESULTS_IN_STRING
-        if (results.size % NUM_OF_RESULTS_IN_STRING > 0) {
-            countOfLines++
+        val answers = solverData.getDoubleArray(RESULT_DATA)
+        for (i in answers!!.indices) {
+            val variable = LayoutInflater.from(requireActivity().applicationContext).inflate(R.layout.result, null)
+            variable.result.text = Html.fromHtml(requireActivity().applicationContext.getString(
+                R.string.var_result,i + 1, answers[i]))
+            results.addView(variable)
         }
-        val resultsForList = Array(countOfLines) { DoubleArray(NUM_OF_RESULTS_IN_STRING) }
-        for (i in results.indices) {
-            resultsForList[i / NUM_OF_RESULTS_IN_STRING][i % NUM_OF_RESULTS_IN_STRING] = results[i]
-        }
-        answersList.adapter = ResultsAnswersAdapter(resultsForList)
     }
 
     companion object {
         const val METHOD = "selected method"
         const val INPUT_DATA = "input data matrix"
         const val RESULT_DATA = "results"
-
-        const val NUM_OF_RESULTS_IN_STRING = 3
     }
 }
